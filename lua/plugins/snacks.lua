8c069852ce15d975ec8c20c366fc625bf3684fbc
Trevor <trevor@hauter.dev>
2025-05-04 11:39:54 -0400

    Disable snacks.indent

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,7 +7,6 @@ return {
         bigfile = { enabled = true },
         dashboard = { enabled = true },
         explorer = { enabled = true },
-        indent = { enabled = true },
         input = { enabled = true },
         notifier = {
             enabled = true,
