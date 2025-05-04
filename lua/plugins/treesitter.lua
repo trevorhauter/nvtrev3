f1e9f2da57b39d2bcd6c08ebd3423b459f124035
Trevor <trevor@hauter.dev>
2025-05-04 14:16:49 -0400

    Add missing parser

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -24,6 +24,7 @@ return {
                 "query",
                 "toml",
                 "typescript",
+                "regex",
                 "vim",
                 "vimdoc",
                 "yaml",
