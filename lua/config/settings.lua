5e10ec1e53738725bc14c2dde8969789763b0f07
Trevor <trevor@hauter.dev>
2025-05-03 20:54:34 -0400

    Add nvim-tree

---
+++ lua/config/settings.lua
@@ -0,0 +1,13 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- TODO: Should this live in the nvim tree config?
+-- == BEGIN NVIM-TREE CONFIG ==
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+--
+-- == END NVIM-TREE CONFIG ==
