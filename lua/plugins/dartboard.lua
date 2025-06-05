2d19d8301580d9139c479c02895414ba8c886770
Trevor <trevor@hauter.dev>
2025-06-04 20:46:35 -0400

    Switch dartboard source

--- lua/plugins/dartboard.lua
+++ lua/plugins/dartboard.lua
@@ -1,22 +1,21 @@
 return {
-	-- TODO: switch this url back over when multi-project lists are added
-	"trevorhauter/dartboard.nvim",
-	dependencies = { "nvim-telescope/telescope.nvim" },
-	config = function()
-		-- Add current file to marks
-		vim.keymap.set("n", "<leader>da", ":DartboardAdd<CR>", { desc = "[M]ark [A]dd file" })
+    "markgandolfo/dartboard.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function()
+        -- Add current file to marks
+        vim.keymap.set("n", "<leader>da", ":DartboardAdd<CR>", { desc = "[M]ark [A]dd file" })
 
-		-- Remove current file from marks
-		vim.keymap.set("n", "<leader>dr", ":DartboardRemove<CR>", { desc = "[M]ark [R]emove file" })
+        -- Remove current file from marks
+        vim.keymap.set("n", "<leader>dr", ":DartboardRemove<CR>", { desc = "[M]ark [R]emove file" })
 
-		-- Show marked files in Telescope
-		vim.keymap.set("n", "<leader>dl", ":DartboardList<CR>", { desc = "[M]ark [S]how files" })
+        -- Show marked files in Telescope
+        vim.keymap.set("n", "<leader>dl", ":DartboardList<CR>", { desc = "[M]ark [S]how files" })
 
-		-- Clear all marks
-		vim.keymap.set("n", "<leader>dc", ":DartboardClear<CR>", { desc = "[M]ark [C]lear all" })
+        -- Clear all marks
+        vim.keymap.set("n", "<leader>dc", ":DartboardClear<CR>", { desc = "[M]ark [C]lear all" })
 
-		require("dartboard").setup({
-			use_default_keymaps = false,
-		})
-	end,
+        require("dartboard").setup({
+            use_default_keymaps = false,
+        })
+    end,
 }
