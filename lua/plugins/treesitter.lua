5b59abc969dac20ddc5b41bc65bf94f2b8df71db
Trevor <trevor@utilityapi.com>
2025-10-16 11:00:29 -0400

    Remove duplicate syntax highlighter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -25,7 +25,6 @@ return {
                 "query",
                 "regex",
                 "toml",
-                "toml",
                 "typescript",
                 "vim",
                 "vimdoc",
